26cb94fe473e2bd7d28e16f2b2965b69a4afcf06
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-10-04 19:28:41 -0400

    Use symbols instead of strings for adapters

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,8 +10,8 @@ require './lib/adapters/custom'
 module Ducalis
   class CLI < Thor
     ADAPTERS = {
-      'circle' => Adapters::CircleCi,
-      'custom' => Adapters::Custom
+      circle: Adapters::CircleCi,
+      custom: Adapters::Custom
     }.freeze
     DEFAULT_ADAPTER = ADAPTERS.keys.last
     default_task :start
@@ -25,7 +25,7 @@ module Ducalis
     option :dry,  type: :boolean, default: false,
                   desc: 'Allows user run dry mode, default: false'
     def start
-      adapter = ADAPTERS.fetch(options[:adapter]) do
+      adapter = ADAPTERS.fetch(options[:adapter].to_sym) do
         raise "Unsupported adapter #{options[:adapter]}"
       end
       Runner.new(adapter.new(options)).call
